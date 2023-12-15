db903d0eb5bda813659109384e0da1dbf2691e93
Joe Sun <dps_swanuk@hotmail.com>
2023-12-15 03:26:13 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Aoc2023day14;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var lines = FileReader.ReadFile("real.txt");
+        Console.WriteLine(lines.Count);
+        var target = 1000000000;
+        var mirrors = new Mirrors(lines);
+        var valuePart1 = mirrors.GetValue(1);
+        var valuePart2 = mirrors.GetValue(target);
+        Console.WriteLine("Aoc 2023 Day 14 part1: " + valuePart1);
+        Console.WriteLine("Aoc 2023 Day 14 part2: " + valuePart2);
+    }
+}
